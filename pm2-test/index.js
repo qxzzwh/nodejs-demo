b756daa50fec886615bfde653d9f9089bca6280a
Coin Qian <qxzzwh@hotmail.com>
2022-04-15 19:15:00 +0800

    pm2 demo completed.

--- pm2-test/index.js
+++ pm2-test/index.js
@@ -1,10 +1,17 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
+    console.log('Current time:', Date.now())
+    console.error('error', Date.now())
+
+    //simulate an error
+    if (req.url === '/err') {
+        throw new Error('/err something wrong')
+    }
     res.setHeader('Content-type', 'application/json')
     res.end(
         JSON.stringify({
             errno: 0,
-            msg: 'pm2 test sever 1'
+            msg: 'pm2 test sever 3'
 
         })
     )
